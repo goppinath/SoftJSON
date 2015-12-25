5e4fa9884f8cd4eee06046787f2eb67d4bbca9ff
Goppinath Thurairajah <goppinath.t@gmail.com>
2015-12-25 12:44:41 +0100

    Initial Commit

---
+++ SoftJSON/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SoftJSON
+//
+//  Created by Goppinath Thurairajah on 25.12.15.
+//  Copyright © 2015 Goppinath Thurairajah. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
